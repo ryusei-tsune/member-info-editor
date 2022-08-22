e17b993ca347daa37059627d3194c4ae8e635765
ryusei-tsune <rt1226.tennis@gmail.com>
2022-08-22 15:30:28 +0900

    React × typescript のセットアップ

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <div className='app-bar'>入力ホーム</div>
+    </div>
+  );
+}
+
+export default App;
