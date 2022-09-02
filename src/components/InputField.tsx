33f68eb2847ff10a751129e61bf96d52a9421c18
ryusei-tsune <rt1226.tennis@gmail.com>
2022-09-02 11:56:22 +0900

    コンポーネント化，型の定義を別ファイルに

---
+++ src/components/InputField.tsx
@@ -0,0 +1,14 @@
+import { Ref, forwardRef } from 'react'
+import { Input } from '../states/type'
+
+export const InputField = forwardRef((props: Input, ref: Ref<HTMLInputElement>) => {
+    const { type, placeholder } = props
+    return (
+        <input
+            type={type}
+            ref={ref}
+            placeholder={placeholder}
+            className='border border-gray-300 text-gray-900 text-sm pl-2 rounded-lg w-full'
+        />
+    )
+})
---
+++ src/states/type.ts
@@ -0,0 +1,24 @@
+export type Member = {
+    id: number,
+    class: number,
+    lname: string,
+    fname: string,
+    elname: string,
+    efname: string,
+    email: string,
+    career1: string,
+    career2: string,
+    career3: string,
+    career4: string,
+    career5: string,
+    study: string,
+    hobby: string,
+    language: string,
+    comment: string,
+  
+  }
+  
+export type Input = {
+    type: string
+    placeholder?: string
+  }
